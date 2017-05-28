c4463d7d90803170e96989f4c74c6fab304e3306
Muhammad Irfan Luthfi <iir_irfan02@hotmail.com>
2017-05-28 10:21:40 +0700

    Source Code for article: Let's Create a Rectangle
    Source Code for article: Let's Create a Rectangle with Java at https://mil-notes.blogspot.com

---
+++ java/Rectangle.java
@@ -0,0 +1,16 @@
+public class Rectangle{
+
+	public static void main(String[] args){
+		int right = 15;
+		int down = 5;
+                                
+                for(int i = 0; i<down; i++){
+                    for(int a = 0; a<right; a++){
+                        System.out.print(" * ");
+                    }
+                    System.out.println();
+                }
+	
+	}
+}
+
